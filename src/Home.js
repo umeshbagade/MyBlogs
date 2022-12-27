f05fa8e0216bdd8e5f86e1c5aebbc85b77346cd9
Umesh Bagade <umeshbagade4321@gmail.com>
2022-12-27 16:37:20 +0530

    Blog List Displayed

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,22 @@ import { useState } from "react"
 
 const Home = () => {
 
-   const [name, setName] = useState("umesh");
-   const [age, setAge] = useState(19)
+    const [blogs, setBlogs] = useState([
+        {   title:"My new Website", body:"Come and visit, hurry up", author:"Umesh", id:1   },
+        {   title:"Welcome Party", body:"Hey buddy lets come for Internship party", author:"Vaishnav", id:2   },
+        {   title:"Placement Party", body:"Guyz, placement is here", author:"Kaustubh", id:3   },
+    ]);
+ 
 
-    const handleClick = (e) =>{
-
-        alert("After 3 years")
-        
-        setName("Umesh");
-        setAge(age+3)
-
-        console.log("button clicked",e, name, age);
-    }
-    const handleClickAgain = (name,e) =>{
-        console.log("hello"+name, e);
-    }
-
+    
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old...</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) =>handleClickAgain("mario", e)}>Click me again</button>
+         {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+         ))}
         </div>
     );
 }
