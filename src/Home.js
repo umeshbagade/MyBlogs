fad4d2fa8ec1aaef91ab6b3011b603dcdbd38f7c
Umesh Bagade <umeshbagade4321@gmail.com>
2022-12-28 16:52:45 +0530

    Reusing componentes With Props and Filtering

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,21 @@
 import { useState } from "react"
+import BlogList from "./BlogList";
 
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
-        {   title:"My new Website", body:"Come and visit, hurry up", author:"Umesh", id:1   },
-        {   title:"Welcome Party", body:"Hey buddy lets come for Internship party", author:"Vaishnav", id:2   },
-        {   title:"Placement Party", body:"Guyz, placement is here", author:"Kaustubh", id:3   },
+        { title: "My new Website", body: "Come and visit, hurry up", author: "Umesh", id: 1 },
+        { title: "Welcome Party", body: "Hey buddy lets come for Internship party", author: "Vaishnav", id: 2 },
+        { title: "Placement Party", body: "Guyz, placement is here", author: "Kaustubh", id: 3 },
     ]);
- 
 
-    
-    return (  
+    return (
         <div className="home">
-         {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            </div>
-         ))}
+           <BlogList blogs = {blogs} title="All Blogs" />
+           <BlogList blogs = {blogs.filter((blog)=> blog.author === 'Umesh')} title="Umesh's Blogs" />
+           <BlogList blogs = {blogs.filter((blog)=> blog.author === 'Vaishnav')} title="Vaishnav's Blogs" />
         </div>
     );
 }
- 
+
 export default Home;
