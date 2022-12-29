a7757df978b8b42173fe57fae3cc31bb899e07c3
Umesh Bagade <umeshbagade4321@gmail.com>
2022-12-29 16:00:20 +0530

    Conditional Loading done

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
+    const [isLoading, seIsLoading] = useState(true)
     useEffect(() => {
+    
+       // done use setTimeout Ever its just for Concept Clarification
+       setTimeout(()=>{         
         fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            seIsLoading(false)
+        });
+       },1000)
     }, [])
 
     return (
         <div className="home">
+            { isLoading && <div style={{ textAlign: 'left'}}>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
