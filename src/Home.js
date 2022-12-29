d71757af783b84256f613ffe483ac63313fb714c
Umesh Bagade <umeshbagade4321@gmail.com>
2022-12-29 17:22:04 +0530

    Custom Hook Done

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,11 @@
-import { useEffect, useState } from "react"
+
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    useEffect(() => {
-
-        // done use setTimeout Ever its just for Concept Clarification
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {                // request on the endpoint which is not present
-                       throw Error('Could not fetch the data for that resourse')
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsLoading(false)
-                    setError(null)
-                })
-                .catch(err => {                   // if unable to fetch i.e. like, server not present
-                   setError(err.message)
-                   setBlogs(null)
-                   setIsLoading(false)
-                })
-        }, 1000)
-    }, [])
+   
+    const { data:blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
+    
 
     return (
         <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+const useFetch = (url) => {
+
+
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+
+        // done use setTimeout Ever its just for Concept Clarification
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {                // request on the endpoint which is not present
+                        throw Error('Could not fetch the data for that resourse')
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsLoading(false)
+                    setError(null)
+                })
+                .catch(err => {                   // if unable to fetch i.e. like, server not present
+                    setError(err.message)
+                    setData(null)
+                    setIsLoading(false)
+                })
+        }, 1000)
+    }, [url])
+
+    return { data, isLoading, error }
+
+}
+
+export default useFetch;
