13ef1d38863cbd87166282580b7834ff02c6bc22
Umesh Bagade <umeshbagade4321@gmail.com>
2022-12-31 19:19:46 +0530

    Input form created | states created

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 const BlogDetails = () => {
 
     const { id } = useParams();
+    const {data:blog , isLoading, error } = useFetch("http://localhost:8000/blogs/"+ id);
     return ( 
         <div className="blog-details">
-            <h2>blog details - {id}</h2>
+            { isLoading && <div>Loading...</div>}
+            { error && <div>{error}</div>}
+            { blog && (
+                <article>
+                    <h2> {blog.title}</h2>
+                    <p> Written By {blog.author}</p>
+                    <div> {blog.body} </div>
+                </article>
+            )}
         </div>
      );
 }
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("umesh")
+    const [body, setBody]   = useState("")
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form >
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value = {title}
+                    onChange = {(e)=>{
+                        setTitle(e.target.value)
+                    }}
+                />
+
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value = {body}
+                    onChange = {(e)=>{
+                        setBody(e.target.value)
+                    }}
+                ></textarea>
+
+                <label >Blog author:</label>
+                <select
+                    onChange={(e)=>{
+                        setAuthor(e.target.value)
+                    }}
+                    value = {author}
+                >
+                    <option value="umesh">Umesh</option>
+                    <option value="vaishnav">Vaishnav</option>
+                </select>
+
+                <button>Add Blog</button>
+                <br />
+             
+            </form>
         </div>
      );
 }
